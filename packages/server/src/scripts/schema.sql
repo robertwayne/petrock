ef8c46b45f1045765afa2d91f0d277040e1fbf16
Rob <devrobw@gmail.com>
2021-03-29 22:49:40 -0400

    Updated DB schema.

--- packages/server/src/scripts/schema.sql
+++ packages/server/src/scripts/schema.sql
@@ -1,9 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS leaderboardsv2
+CREATE TABLE IF NOT EXISTS players
+(
+    id uuid DEFAULT uuid_generate_v4 (),
+    username VARCHAR(255) UNIQUE PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS leaderboards
 (
     id uuid DEFAULT uuid_generate_v4(),
-    username VARCHAR(255) UNIQUE NOT NULL,
+    player VARCHAR(255) UNIQUE NOT NULL,
+    CONSTRAINT fk_player FOREIGN KEY (player) REFERENCES players (username),
 
     /* Experience Data */
     total_experience INT NOT NULL,
@@ -16,14 +23,17 @@ CREATE TABLE IF NOT EXISTS leaderboardsv2
     last_modified TIMESTAMP DEFAULT now() NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS player
-(
-    id uuid DEFAULT uuid_generate_v4 (),
-    username VARCHAR(255) UNIQUE PRIMARY KEY NOT NULL
-);
-
-/* History represents a series of leaderboard experience data. Each row is constrained to a player and contains time */
+/* History represents a series of leaderboard experience data. Each row is constrained
+   to a player and contains time and the experience earned that day.*/
 CREATE TABLE IF NOT EXISTS history
 (
+    id uuid DEFAULT uuid_generate_v4(),
+    player VARCHAR(255) UNIQUE NOT NULL,
+    CONSTRAINT fk_player FOREIGN KEY (player) REFERENCES players (username),
 
+    experience INT NOT NULL,
+    place INT NOT NULL,
+
+    /* Meta Data */
+    created_on TIMESTAMP DEFAULT now() NOT NULL
 );
