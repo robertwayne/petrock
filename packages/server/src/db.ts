9464f89e676e75f0241f8aac2ed8dd27ce9a0368
Rob <devrobw@gmail.com>
2021-03-30 00:58:38 -0400

    Updated old debug values.

--- packages/server/src/db.ts
+++ packages/server/src/db.ts
@@ -4,11 +4,11 @@ require('dotenv').config()
 
 
 export const pool: Pool = new Pool({
-    user: process.env.NODE_ENV === 'production' ? process.env.PGUSER : 'test_admin',
+    user: process.env.NODE_ENV === 'production' ? process.env.PGUSER : 'postgres',
     host: process.env.NODE_ENV === 'production' ? process.env.PGHOST : 'localhost',
-    database: process.env.NODE_ENV === 'production' ? process.env.PGDATABASE : 'leaderboards',
-    password: process.env.NODE_ENV === 'production' ? process.env.PGPASSWORD : 'test123',
+    database: process.env.NODE_ENV === 'production' ? process.env.PGDATABASE : 'postgres',
+    password: process.env.NODE_ENV === 'production' ? process.env.PGPASSWORD : 'postgres',
     port: process.env.NODE_ENV === 'production' ? Number(process.env.PGPORT) : 5432,
 })
 
-export const DB_CONNECTION_STRING: string = `postgresql://test_admin:test123@localhost:5432/leaderboards`
+export const DB_CONNECTION_STRING: string = `postgresql://postgres:postgres@localhost:5432/postgres`
