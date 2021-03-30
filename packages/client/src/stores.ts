6a9d1d1936ce7894f5438b02321319d492ec42df
Rob <devrobw@gmail.com>
2021-03-30 02:16:42 -0400

    Updated typing for updateTimer store.

--- packages/client/src/stores.ts
+++ packages/client/src/stores.ts
@@ -3,4 +3,4 @@ import { Writable, writable } from 'svelte/store'
 export const leaderboard: Writable<Array<Player>> = writable([])
 export const sortBy: Writable<string> = writable('')
 export const sortAsc: Writable<boolean> = writable(false)
-export const updateTimer: Writable<any> = writable([])
+export const updateTimer: Writable<number> = writable(0)
