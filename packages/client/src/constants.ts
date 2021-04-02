370aa89a0db1039f5bed318984755f9739d1e0e7
Rob <devrobw@gmail.com>
2021-04-02 13:23:00 -0400

    Temporary solution to snowpack bug.

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,2 +1,6 @@
 export const tickRate = import.meta.env.TICK_RATE
-export const url = import.meta.env.MODE === 'production' ? import.meta.env.API_BASE_URL : 'http://localhost:3000'
+// export const url = import.meta.env.MODE === 'production' ? import.meta.env.API_BASE_URL : 'http://localhost:3000'
+// snowpack v3+ is not setting mode/node_env properly w/ build
+// can uncomment when it is fixed...
+// see: https://github.com/snowpackjs/snowpack/issues/3053
+export const url = 'https://petrock.gg'
