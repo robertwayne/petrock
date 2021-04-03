2f09feb0e423f4d8633c13d2100ba7fd18d2ba5c
Rob <devrobw@gmail.com>
2021-04-02 23:07:24 -0400

    Add item enums.
    Add item enums.

---
+++ packages/client/src/enums.ts
@@ -0,0 +1,17 @@
+export enum ItemSlot {
+    HEAD = 1,
+    BODY = 2,
+    MAIN_HAND = 3,
+    OFF_HAND = 4,
+    MASK = 5,
+    OUTFIT = 6,
+    CLOTHES_DYE = 7,
+    HAIR_DYE = 8
+} 
+
+export enum ItemType {
+    EQUIPMENT = 1,
+    COSMETIC = 2,
+    CONSUMABLE = 3,
+    MISC = 4
+}
