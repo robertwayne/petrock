09cc2ed7ff52e2a6b21ad05fce50e14fd12919ad
Rob <rob@robwagner.dev>
2021-04-02 21:23:38 -0400

    Add Item type.

--- packages/shared/types/index.d.ts
+++ packages/shared/types/index.d.ts
@@ -12,3 +12,22 @@ export interface RawPlayerData {
     username: string
     experience: number
 }
+
+export interface Item {
+    name: string
+    description?: string
+    level?: number
+    slot?: string
+    type: string
+    consumable?: boolean
+    usable_by?: string
+    strength?: number
+    intelligence?: number
+    agility?: number
+    defense?: number
+    wisdom?: number
+    luck?: number
+    can_sell: boolean
+    sell_price?: number
+    icon: string
+}
