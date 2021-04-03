4f1300554ca9edafaf4dce311aefd07b93744601
Rob <devrobw@gmail.com>
2021-04-02 22:06:41 -0400

    yarn format

--- packages/client/src/routeHandler.ts
+++ packages/client/src/routeHandler.ts
@@ -4,9 +4,9 @@ import { wrap } from 'svelte-spa-router/wrap'
 export const routes = {
     '/': Home,
     '/items': wrap({
-        asyncComponent: () => import('./routes/Items.svelte')
+        asyncComponent: () => import('./routes/Items.svelte'),
     }),
     '*': wrap({
-        asyncComponent: () => import('./routes/NotFound.svelte')
-    })
+        asyncComponent: () => import('./routes/NotFound.svelte'),
+    }),
 }
