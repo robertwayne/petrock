a0138e882260bb18134b523f93d52b3c54433fc1
Rob <devrobw@gmail.com>
2021-04-03 22:34:50 -0400

    Updated all item data.

--- packages/server/migrations/up_000.sql
+++ packages/server/migrations/up_000.sql
@@ -103,28 +103,29 @@ VALUES
 (7, 'Clothes Dye'),
 (8, 'Hair Dye');
 
-INSERT INTO items (name, description, level, slot, type, consumable, usable_by, strength, intelligence, agility, defense, wisdom, luck, can_sell, sell_price)
+INSERT INTO items (name, description, level, slot, type, consumable, usable_by, strength,
+                   intelligence, agility, defense, wisdom, luck, can_sell, sell_price)
 VALUES
-('Cypress Stick', NULL, 1, 3, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Oaken Club', NULL, 2, 3, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Leather Cap', NULL, 1, 1, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Training Sword', NULL, 4, 3, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Training Wand', NULL, 4, 3, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Leather Armor', NULL, 3, 2, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Padded Garb', NULL, 3, 2, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Tattered Cloak', NULL, 2, 2, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Wooden Shield', NULL, 2, 4, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Simple Bracelet', NULL, 2, 4, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
+('Cypress Stick', NULL, 1, 3, 1, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 1),
+('Oaken Club', NULL, 2, 3, 1, false, (true, false, true), 2, 0, 0, 0, 0, 0, true, 2),
+('Leather Cap', NULL, 1, 1, 1, false, (true, true, true), 0, 0, 0, 1, 0, 0, true, 1),
+('Training Sword', NULL, 4, 3, 1, false, (true, false, false), 5, 0, 0, 0, 0, 0, true, 45),
+('Training Wand', NULL, 4, 3, 1, false, (false, true, true), 1, 3, 0, 0, 0, 0, true, 52),
+('Leather Armor', NULL, 3, 2, 1, false, (true, false, false), 0, 0, 0, 3, 0, 0, true, 24),
+('Padded Garb', NULL, 3, 2, 1, false, (false, false, true), 0, 0, 0, 2, 1, 0, true, 18),
+('Tattered Cloak', NULL, 2, 2, 1, false, (false, false, true), 0, 0, 0, 1, 1, 0, true, 12),
+('Wooden Shield', NULL, 2, 4, 1, false, (true, false, false), 0, 0, 0, 2, 0, 0, true, 1),
+('Simple Bracelet', NULL, 2, 4, 1, false, (false, true, true), 0, 0, 0, 1, 1, 0, true, 1),
 
-('Health Potion', 'Restores 30 HP to an ally.', NULL, NULL, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Mana Potion', 'Restores 20 MP to an ally.', NULL, 3, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Teleport Scroll', 'Return the party to the last visited town.', NULL, NULL, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
+('Health Potion', 'Restores 30 HP to an ally.', NULL, NULL, 3, true, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 3),
+('Mana Potion', 'Restores 20 MP to an ally.', NULL, 3, 3, true, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 7),
+('Teleport Scroll', 'Return the party to the last visited town.', NULL, NULL, 3, true, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 10),
 
-('Pet Rock', 'It literally does nothing.', 1, 3, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Sunglasses', 'Deal with it.', 1, 3, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Pink Hair Dye', 'Valentine''s Day hair dye.', 1, 3, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Princess Crown', 'Valentine''s Day mask.', 1, 3, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Devil Horns', 'Valentine''s Day mask.', 1, 3, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Rockstar Hair', 'Valentine''s Day mask.', 1, 3, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0),
-('Messy Hair', 'Just the right amount of messy.', 1, 3, 3, false, (true, true, true), 1, 0, 0, 0, 0, 0, true, 0)
+('Pet Rock', 'It literally does nothing.', 1, 3, 3, false, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 1),
+('Sunglasses', 'Deal with it.', 1, 3, 3, false, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 1),
+('Pink Hair Dye', 'Valentine''s Day hair dye.', 1, 3, 3, false, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 1),
+('Princess Crown', 'Valentine''s Day mask.', 1, 3, 3, false, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 1),
+('Devil Horns', 'Valentine''s Day mask.', 1, 3, 3, false, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 1),
+('Rockstar Hair', 'Valentine''s Day mask.', 1, 3, 3, false, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 1),
+('Messy Hair', 'Just the right amount of messy.', 1, 3, 3, false, (true, true, true), NULL, NULL, NULL, NULL, NULL, NULL, true, 1)
 ;
