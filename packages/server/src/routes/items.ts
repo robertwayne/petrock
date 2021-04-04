a004a7157dc121b93607976adf9ed8ecf6a2acb2
Rob <devrobw@gmail.com>
2021-04-03 22:17:46 -0400

    Updated items API.

--- packages/server/src/routes/items.ts
+++ packages/server/src/routes/items.ts
@@ -1,19 +1,29 @@
 import { FastifyInstance, FastifyRequest } from 'fastify'
 
-// type LeaderboardRequest = FastifyRequest<{
-//     Querystring: {
-//         order: string
-//         sort: string
-//     }
-//}>
+type ItemsRequest = FastifyRequest<{
+    Querystring: {
+        order: string
+        asc: string
+        search: string
+        slot: string
+        type: string
+        classes: string
+        level: number
+    }
+}>
 
 export const routeItems = async (app: FastifyInstance): Promise<void> => {
-    app.get('/items', {}, async (request: FastifyRequest) => {
+    app.get('/items', {}, async (request: ItemsRequest) => {
         const client = await app.pg.connect()
 
         const { rows } = await client.query(
             `
-            SELECT *
+            SELECT 
+            (
+                i.name, i.name, i.description, i.level, i.slot, i.type, i.consumable, i.usable_by,
+                i.strength, i.intelligence, i.agility, i.defense, i.wisdom, i.luck, i.can_sell, i.sell_price,
+                i.icon
+            )
             FROM items i;
             `
         )
