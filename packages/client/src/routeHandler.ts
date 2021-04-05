d07974975d272b0627e4138c26d21b00add49183
Rob <devrobw@gmail.com>
2021-04-05 14:09:28 -0400

    Added missing routes.

--- packages/client/src/routeHandler.ts
+++ packages/client/src/routeHandler.ts
@@ -3,8 +3,27 @@ import { wrap } from 'svelte-spa-router/wrap'
 
 export const routes = {
     '/': Home,
+    '/leaderboard': Home,
     '/items': wrap({
         asyncComponent: () => import('./routes/Items.svelte'),
+    }),
+    '/classes': wrap({
+        asyncComponent: () => import('./routes/Classes.svelte'),
+    }),
+    '/events': wrap({
+        asyncComponent: () => import('./routes/Events.svelte'),
+    }),
+    '/monsters': wrap({
+        asyncComponent: () => import('./routes/Monsters.svelte'),
+    }),
+    '/skills': wrap({
+        asyncComponent: () => import('./routes/Skills.svelte'),
+    }),
+    '/zones': wrap({
+        asyncComponent: () => import('./routes/Zones.svelte'),
+    }),
+    '/tracker': wrap({
+        asyncComponent: () => import('./routes/Tracker.svelte'),
     }),
     '*': wrap({
         asyncComponent: () => import('./routes/NotFound.svelte'),
