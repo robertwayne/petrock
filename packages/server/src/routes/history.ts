5695441e9f2aed992f121f3f922c8a51454c41df
Rob <devrobw@gmail.com>
2021-04-12 21:12:41 -0400

    Add all param to history endpoint. Return trunc created_on data.

--- packages/server/src/routes/history.ts
+++ packages/server/src/routes/history.ts
@@ -3,6 +3,7 @@ import { FastifyInstance, FastifyRequest } from 'fastify'
 type HistoryRequest = FastifyRequest<{
     Querystring: {
         username: string
+        all: boolean
     }
 }>
 
@@ -11,23 +12,40 @@ export const routeHistory = async (app: FastifyInstance): Promise<void> => {
     app.get('/history', {}, async (request: HistoryRequest) => {
         const client = await app.pg.connect()
 
-        const { rows } = await client.query(
-            `
-            SELECT 
-                p.username, h.experience
-            FROM history h
-            INNER JOIN players p ON (h.player = p.username)
-            WHERE h.player = $1 AND h.created_on = CURRENT_DATE;
-            `,
-            [request.query.username]
-        )
+        if (!request.query.all) {
+            const { rows } = await client.query(
+                `
+                SELECT 
+                    p.username, h.experience, to_char(h.created_on, 'mm-dd-yyyy') AS created_on
+                FROM history h
+                INNER JOIN players p ON (h.player = p.username)
+                WHERE h.player = $1 AND h.created_on = CURRENT_DATE;
+                `,
+                [request.query.username]
+            )
 
-        client.release()
+            client.release()
 
-        return rows.length > 0
-            ? rows[0]
-            : {
-                  error: 'No results found.',
-              }
+            return rows.length > 0
+                ? rows[0]
+                : {
+                      error: 'No results found.',
+                  }
+        } else {
+            const { rows } = await client.query(
+                `
+                SELECT 
+                    h.player, h.experience, to_char(h.created_on, 'mm-dd-yyyy') AS created_on
+                FROM history h
+                WHERE h.player = $1
+                ORDER BY h.created_on DESC;
+                `,
+                [request.query.username]
+            )
+    
+            client.release()
+    
+            return rows
+        }
     })
 }
