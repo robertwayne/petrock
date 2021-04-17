1763bf38e6f2bfd7706db1f85e021a3cc5ab1923
Rob <rob@robwagner.dev>
2021-04-16 22:46:23 -0400

    Cleaned up local dev code.

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -29,6 +29,7 @@ if (process.env.NODE_ENV === 'development') {
         contentSecurityPolicy: {
             directives: {
                 'default-src': '*',
+                'style-src': ["'self'", 'fonts.googleapis.com', "'unsafe-inline'"],
             },
         },
     })
